3d63530c2e5337ca413506ee1ae562afcb840538
Ochiengmichael2024 <ochiengmichael082@gmail.com>
2026-02-27 10:53:04 +0300

    fix the css file for animated effect

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const wrapper= document.querySelector('wrapper')
+const wrapper= document.querySelector('.wrapper')
 const signUplink= document.querySelector('.signUp-link')
 const signInlink= document.querySelector('.signIn-link')
 
 signUplink.addEventListener('click', ()=>{
+ wrapper.classList.toggle('show-sign');
+});
+
+signInlink.addEventListener('click', ()=>{
  wrapper.classList.toggle('show-sign');
 });
